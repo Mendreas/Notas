b8de8e0d68630ac291eba67dd93e64458992e44b
Mendreas <mendreas@icloud.com>
2026-08-25 13:19:22 -0300

    v0.6.7: bump PWA cache for Mexico and South Africa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="notas-mundo-v066";
+const CACHE="notas-mundo-v067";
 const CORE=["/","/index.html","/manifest.webmanifest","/assets/icon.svg"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener("activate",event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)));await self.clients.claim()})())});
