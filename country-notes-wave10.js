1c7840c7d03d53ebcfed39ae3d73481644e9a4bd
Mendreas <mendreas@icloud.com>
2026-08-27 20:01:20 -0300

    Fix SEK 1000 official Riksbank image paths

--- country-notes-wave10.js
+++ country-notes-wave10.js
@@ -10,8 +10,8 @@
     50:['https://www.riksbank.se/contentassets/c2b34515579241d5a211fcfcd89f1dc9/50-kronorssedel-navigation-specimen-fram.png','https://www.riksbank.se/contentassets/c2b34515579241d5a211fcfcd89f1dc9/50-kronorssedel-navigation-specimen-bak.png'],
     100:['https://www.riksbank.se/contentassets/ec0fcae299064c80871e8ccf3aa8e89b/100-kronorssedel-specimen-fram.png','https://www.riksbank.se/contentassets/ec0fcae299064c80871e8ccf3aa8e89b/100-kronorssedel-specimen-bak.png'],
     200:['https://www.riksbank.se/contentassets/8bd04134e5c046b38232d355d9bb8a75/200-kronorssedel-specimen-fram.png','https://www.riksbank.se/contentassets/8bd04134e5c046b38232d355d9bb8a75/200-kronorssedel-specimen-bak.png'],
-    500:['https://www.riksbank.se/contentassets/e6e9874c29a44edc96d38442e173865d/500-kronorssedel-specimen-fram.png','https://www.riksbank.se/contentassets/e6e9874c29a44edc96d38442e173865d/500-kronossedel-specimen-bak.png'],
-    1000:['https://www.riksbank.se/contentassets/352d05bdefae4a7387f5bf27a795b4f2/1000-kronossedel-specimen-fram.png','https://www.riksbank.se/contentassets/352d05bdefae4a7387f5bf27a795b4f2/1000-kronosssedel-specimen-bak.png']
+    500:['https://www.riksbank.se/contentassets/e6e9874c29a44edc96d38442e173865d/500-kronorssedel-specimen-fram.png','https://www.riksbank.se/contentassets/e6e9874c29a44edc96d38442e173865d/500-kronorssedel-specimen-bak.png'],
+    1000:['https://www.riksbank.se/contentassets/352d05bdefae4a7387f5bf27a795b4f2/1000-kronorssedel-specimen-fram.png','https://www.riksbank.se/contentassets/352d05bdefae4a7387f5bf27a795b4f2/1000-kronorssedel-specimen-bak.png']
   };
   const pln={};
   [10,20,50,100,200,500].forEach(v=>pln[v]=[
@@ -24,6 +24,7 @@
     `https://www.cnb.cz/export/sites/cnb/cs/bankovky-a-mince/.galleries/bankovky/galerie_obrazku/${v}_rub.jpg`
   ]);
   const dims={
+    SEK:{20:'120 × 66 mm',50:'126 × 66 mm',100:'133 × 66 mm',200:'140 × 66 mm',500:'147 × 66 mm',1000:'154 × 66 mm'},
     PLN:{10:'120 × 60 mm',20:'126 × 63 mm',50:'132 × 66 mm',100:'138 × 69 mm',200:'144 × 72 mm',500:'150 × 75 mm'},
     CZK:{100:'140 × 69 mm',200:'146 × 69 mm',500:'152 × 69 mm',1000:'158 × 74 mm',2000:'164 × 74 mm',5000:'170 × 74 mm'},
     RON:{1:'120 × 62 mm',5:'127 × 67 mm',10:'133 × 72 mm',20:'136 × 77 mm',50:'140 × 77 mm',100:'147 × 82 mm',200:'150 × 82 mm',500:'153 × 82 mm'}
