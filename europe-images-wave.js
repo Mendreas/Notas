f0290bddb2aa2e1eef5e95dd98a56b1d0852caff
Mendreas <mendreas@icloud.com>
2026-08-28 12:13:52 -0300

    Add verified HUF UAH and ALL banknote images

--- europe-images-wave.js
+++ europe-images-wave.js
@@ -10,12 +10,31 @@
     'RSD:1000':['1000RSD front.jpg','1000RSD reverse.jpg'],
     'RSD:2000':['2000RSD front.jpg','2000RSD revers.jpg'],
     'RSD:5000':['5000 front.jpg','5000 back.jpg'],
-    'MDL:1':['1 leu Moldova 2010 obverse.jpg','1 leu Moldova 2010 reverse.jpg'],
+
     'BYN:10':['10 Belarus 2009 front.jpg','10 Belarus 2009 back.jpg'],
     'BYN:20':['20 Belarus 2009 front.jpg','20 Belarus 2009 back.jpg'],
     'BYN:50':['50 Belarus 2009 front.jpg','50 Belarus 2009 back.jpg'],
     'BYN:100':['100 Belarus 2009 front.jpg','100 Belarus 2009 back.jpg'],
-    'BYN:500':['500 Belarus 2009 front.jpg','500 Belarus 2009 back.jpg']
+    'BYN:500':['500 Belarus 2009 front.jpg','500 Belarus 2009 back.jpg'],
+
+    'MDL:1':['1 leu Moldova 2010 obverse.jpg','1 leu Moldova 2010 reverse.jpg'],
+
+    'HUF:500':['500 forint elolap.png','500 forint hatlap.png'],
+    'HUF:1000':['HUF 1000 2018 obverse.png','HUF 1000 2018 reverse.png'],
+    'HUF:2000':['2000 HUF 2017 ob.jpg','2000 HUF 2017 rev.jpg'],
+    'HUF:5000':['5000 HUF 2017 ob.jpg','5000 HUF 2017 rev.jpg'],
+    'HUF:10000':['10000 HUF 2014 ob.jpg','10000 HUF 2014 rev.jpg'],
+    'HUF:20000':['20000 HUF 2015 ob.png','20000 HUF 2015 rev.png'],
+
+    'ALL:1000':['ALBW2019-01000o.jpg','1000lek-768x383.png'],
+    'ALL:10000':['ALBW2019-10000o.jpg','ALBW2019-10000r.jpg'],
+
+    'UAH:20':['20 hryvnia 2018 front.jpg','20 hryvnia 2018 back.jpg'],
+    'UAH:50':['50-uah-2019-1.png','50-uah-2019-2.png'],
+    'UAH:100':['100 Ukrainian hryvnia in 2014 Obverse.jpg','100 Ukrainian hryvnia in 2014 Reverse.jpg'],
+    'UAH:200':['200 hryvnia 2019 obverse.jpg','200 hryvnia 2019 reverse.jpg'],
+    'UAH:500':['500 hryvnia 2015 obverse.jpg','500 hryvnia 2015 back.jpg'],
+    'UAH:1000':['1000 UAH 2019 recto.png','1000 UAH 2019 verso.png']
   };
   const previousFetch=window.fetch.bind(window);
   window.fetch=async(...args)=>{
