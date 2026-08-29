743ec17fe3ffe5b0180b68c90f1768cf02c8d034
Mendreas <mendreas@icloud.com>
2026-08-29 17:45:45 +0100

    Fix broken Guyana images and remove incorrect Venezuela note

--- south-america-images-wave.js
+++ south-america-images-wave.js
@@ -1,17 +1,13 @@
 (() => {
   const commons=(name)=>`https://commons.wikimedia.org/wiki/Special:Redirect/file/${encodeURIComponent(name)}?width=1200`;
   const images={
-    VES:{
-      50:[commons('50-Bolívares-anverso.jpg'),commons('50-Bolívares-reverso.jpg')]
-    },
     GYD:{
       20:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/20_fr3_small.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/20_bk3_small.jpg'],
       50:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/50-specimen-front.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/50-specimen-back.jpg'],
       100:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/100_front_specimen.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/100_back_specimen.jpg'],
-      500:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/500-front.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/500-back.jpg'],
+      500:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/500_fr1_small.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/500_bk1_small.jpg'],
       1000:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/1000_2019_front.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/1000_2019_back.jpg'],
-      2000:['https://bankofguyana.org.gy/bog/images/2000-frontsmall.png','https://bankofguyana.org.gy/bog/images/2000-backsmall.png'],
-      5000:['https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/5000-front.jpg','https://bankofguyana.org.gy/bog/images/operations/banking/notescoins/5000-back.jpg']
+      2000:['https://bankofguyana.org.gy/bog/images/2000-frontsmall.png','https://bankofguyana.org.gy/bog/images/2000-backsmall.png']
     }
   };
   const partial={
@@ -21,7 +17,6 @@
     }
   };
   const sources={
-    VES:'https://commons.wikimedia.org/wiki/Category:Banknotes_of_Venezuela',
     GYD:'https://bankofguyana.org.gy/bog3/core-functions/issuance-of-currency/notes',
     SRD:'https://commons.wikimedia.org/wiki/Category:Banknotes_of_Suriname'
   };
@@ -34,8 +29,7 @@
     Object.entries(images).forEach(([code,values])=>Object.entries(values).forEach(([value,pair])=>{
       const n=data.find(x=>x.currency===code&&Number(x.value)===Number(value));
       if(!n)return;
-      const official=code==='GYD';
-      Object.assign(n,{front:pair[0],back:pair[1],imageStatus:official?'official-source':'commons-reusable',imageSource:official?'Bank of Guyana':'Wikimedia Commons',imageSourceUrl:sources[code]});
+      Object.assign(n,{front:pair[0],back:pair[1],imageStatus:'official-source',imageSource:'Bank of Guyana',imageSourceUrl:sources[code]});
     }));
     Object.entries(partial).forEach(([code,values])=>Object.entries(values).forEach(([value,parts])=>{
       const n=data.find(x=>x.currency===code&&Number(x.value)===Number(value));
