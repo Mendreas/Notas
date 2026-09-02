d1a9a98107f5409e21b4ca46e8e95d6d2a0b9a9e
Mendreas <mendreas@icloud.com>
2026-09-02 15:58:39 +0100

    Use local Bank Note Museum scans for all current EGP notes

--- app-preflight.js
+++ app-preflight.js
@@ -2,15 +2,8 @@ document.write('<link rel="stylesheet" href="/glossary.css?v=172"><link rel="sty
 (() => {
   const nativeFetch=window.fetch.bind(window);
   const imported={
-    BWP:new Set([20,50,100,200]),NAD:new Set([10,20,50,100,200]),ZAR:new Set([10,20,50,100,200]),MAD:new Set([20,50,100,200]),KES:new Set([50,100,200,500,1000]),EGP:new Set([5,50]),
+    BWP:new Set([20,50,100,200]),NAD:new Set([10,20,50,100,200]),ZAR:new Set([10,20,50,100,200]),MAD:new Set([20,50,100,200]),KES:new Set([50,100,200,500,1000]),EGP:new Set([5,10,20,50,100,200]),
     GMD:new Set([5,10,20,50,100,200]),SSP:new Set([1,5,10,25,50,100,500,1000]),LYD:new Set([1,5,10,20]),SOS:new Set([1000]),ZWG:new Set([10,20,50])
-  };
-  const commons=f=>'https://commons.wikimedia.org/wiki/Special:Redirect/file/'+encodeURIComponent(f);
-  const egpFallback={
-    10:[commons('10 EGP obverse 2014-8-13.jpg'),commons('10 EGP reverse 2014-8-13.jpg')],
-    20:[commons('20 EGP 2022 Polymer - front 01.jpg'),commons('20 EGP 2022 Polymer - rear.jpg')],
-    100:[commons('100 EGP obverse 2014-1-26.jpg'),commons('100 EGP reverse 2014-1-26.jpg')],
-    200:[commons('200 EGP obverse 2010-1-2.jpg'),commons('200 EGP reverse 2010-1-2.jpg')]
   };
   const placeholderFront='/assets/notes/placeholder-front.svg',placeholderBack='/assets/notes/placeholder-back.svg';
   window.fetch=async(input,init)=>{
@@ -28,10 +21,6 @@ document.write('<link rel="stylesheet" href="/glossary.css?v=172"><link rel="sty
         n.back=`/assets/notes/banknotews/${n.currency.toLowerCase()}/${value}-back.jpg`;
         n.imageStatus='local-reference';
         n.imageSource='Bank Note Museum · cópia local';
-      } else if(n.currency==='EGP'&&egpFallback[value]){
-        [n.front,n.back]=egpFallback[value];
-        n.imageStatus='public-domain';
-        n.imageSource='Central Bank of Egypt / Wikimedia Commons';
       } else {
         if(/^https:\/\/www\.banknote\.ws\//i.test(n.front||''))n.front=placeholderFront;
         if(/^https:\/\/www\.banknote\.ws\//i.test(n.back||''))n.back=placeholderBack;
